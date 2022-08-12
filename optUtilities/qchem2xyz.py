af3525e196d5ede8bfa76055f27ecda4f852e191
Adrea Snow (Monash University) <asnow@monarch-login1.erc.monash.edu>
2022-08-12 19:26:34 +1000

    found an error when a new hessian is generated

--- optUtilities/qchem2xyz.py
+++ optUtilities/qchem2xyz.py
@@ -1,7 +1,5 @@
 #! python3
 import argparse
-
-
 def read_args():
     parser = argparse.ArgumentParser(
         description=(
@@ -35,43 +33,42 @@ def read_args():
 args = read_args()
 
 for file in args.files:
-	with open(file, 'r') as f:
-		lines = f.readlines()
+    with open(file, 'r') as f:
+        lines = f.readlines()
 
-	geomLen = 0
-	xyzList = []
-	xyzPosList = []
-	for count, line in enumerate(lines):
-		if '$molecule' in line:
-			geomStart = count
-		if '$end' in line and geomLen == 0:
-			geomEnd = count
-			geomLen = geomEnd-geomStart-2
+    geomLen = 0
+    xyzList = []
+    xyzPosList = []
+    for count, line in enumerate(lines):
+        if '$molecule' in line:
+            geomStart = count
+        if '$end' in line and geomLen == 0:
+            geomEnd = count
+            geomLen = geomEnd-geomStart-2
 
-		if 'New Cartesian Coordinates Obtained' in line and geomLen != 0:
-			xyzBuffer = ''
-			xyzPosList += [count + 7]
+        if 'New Cartesian Coordinates Obtained' in line and geomLen != 0:
+            xyzBuffer = ''
+            xyzPosList += [count + 7]
 
-	if args.first != [0]:
-		geomLen = args.first[0]
+    if args.first != [0]:
+        geomLen = args.first[0]
 
-	xyzBuffer = ''
-	for xyzPos in xyzPosList:
-		xyzBuffer += f'{geomLen}\n\n'
-		for i in range(xyzPos, (xyzPos + geomLen)):
-			line = lines[i]
-			xyzBuffer += f'{line.split()[1]} {float(line.split()[2]): >20.12f} {float(line.split()[3]): >20.12f} {float(line.split()[4]): >20.12f}\n'
+    xyzOut = ''
+    for xyzPos in xyzPosList:
+        try:
+            xyzBuffer = ''
+            xyzBuffer += f'{geomLen}\n\n'
+            for i in range(xyzPos, (xyzPos + geomLen)):
+                line = lines[i].split()
+                xyzBuffer += f'{line[1]} {float(line[2]): >20.12f} {float(line[3]): >20.12f} {float(line[4]): >20.12f}\n'
+            xyzOut += xyzBuffer
+        except:
+            pass
 
-	if args.out[0] == '':
-		outFile = f'{file.split(".")[0]}.xyz'
-	else:
-		outFile = args.out[0]
+    if args.out[0] == '':
+        outFile = f'{file.split(".")[0]}.xyz'
+    else:
+        outFile = args.out[0]
 
-
-	with open(outFile, 'w+') as f:
-		f.write(xyzBuffer)
-
-
-
-
-
+    with open(outFile, 'w+') as f:
+        f.write(xyzOut)
