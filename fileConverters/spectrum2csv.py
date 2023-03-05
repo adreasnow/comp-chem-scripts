c083f2d074a04825410bf276102a3c78fd8bb0ec
Adrea Snow (Monash University) <asnow@monarch-login1.erc.monash.edu>
2023-03-05 11:29:48 +1100

    new script to convert .spectrum files to .csv

---
+++ fileConverters/spectrum2csv.py
@@ -0,0 +1,144 @@
+#! python3
+import numpy as np
+import argparse
+
+def read_args() -> argparse.ArgumentParser.parse_args:
+    parser = argparse.ArgumentParser(
+        description=(
+            "Script for converting ORCA .spectrum files to .csv"
+        )
+    )
+    parser.add_argument(
+        "-ei",
+        "--energyIn",
+        default=['ev'],
+        help="Units of energy to in spectrum (nm/[ev]/w)",
+        nargs=1,
+        required=False,
+    )
+    parser.add_argument(
+        "-eo",
+        "--energyOut",
+        default=['nm'],
+        help="Units of energy to plot ([nm]/ev/w)",
+        nargs=1,
+        required=False,
+    )
+    parser.add_argument(
+        "-t",
+        "--total",
+        default=False,
+        help="Plot total energy",
+        required=False,
+        action="store_true"
+    )
+    parser.add_argument(
+        "--ht",
+        default=False,
+        help="Plot Herzberg-Teller contribution",
+        required=False,
+        action="store_true"
+    )
+    parser.add_argument(
+        "--fc",
+        default=False,
+        help="Plot Franck-Condon contribution",
+        required=False,
+        action="store_true"
+    )
+    parser.add_argument(
+        "-x",
+        "--xrange",
+        default=None,
+        help="Range of x values to write out default = 200 800",
+        nargs=2,
+        type=float,
+        required=False,
+    )
+    parser.add_argument(
+        'files', 
+        nargs=argparse.REMAINDER
+    )
+    return parser.parse_args()
+
+def evToNm(eV: float | list[float], error: float = 0.0) -> float | tuple[float, float, float]:
+    '''Converts eV values to nm'''
+    def ev2nm(eV):
+        h = 4.135667e-15
+        c = 2.99792e8
+        return np.divide(np.multiply(h, c), np.multiply(eV, 1e-9))
+
+    if type(eV) == float:
+        nm = ev2nm([eV, eV + error, eV - error])
+        if error == 0:
+            return nm[0]
+        elif error != 0:
+            return (nm[0], nm[1], nm[2])
+
+    else:
+        nm = ev2nm(eV)
+        return nm
+
+def readSpectrum(infile: str, spectrumUnits: str, energyUnits: str) -> dict[float]:
+    energy = []
+    total = []
+    fc = []
+    ht = []
+    with open(infile, "r") as f:
+        lines = f.readlines()
+    for line in lines[1:]:
+        vals = line.split()
+        energy += [float(vals[0])]
+        total += [float(vals[1])]
+        fc += [float(vals[2])]
+        ht += [float(vals[3])]
+    if energyUnits == 'nm':
+        if spectrumUnits == 'ev':
+            energy = np.array(evToNm(energy))
+        elif spectrumUnits == 'w':
+            energy = np.multiply(np.divide(1, energy), 1e7)
+    if energyUnits == 'ev':
+        if spectrumUnits == 'ev':
+            energy = np.array(energy)
+        elif spectrumUnits == 'w':
+            energy = np.divide(1.23984198e-6, np.divide(np.divide(1, energy), 100))
+    if energyUnits == 'w':
+        energy = np.array(energy)
+
+    returnDict = {'e': energy, 't': total, 'fc': fc, 'ht': ht}
+
+    return returnDict
+
+def main():
+    args = read_args()
+
+    if args.xrange == None:
+        xrange = [1.54, 6.19] if args.energyOut[0] == 'ev'\
+            else [200, 800] if args.energyOut[0] == 'nm'\
+            else [12421, 49926]
+    else:
+        xrange = args.xrange
+
+    if args.files == None:
+        print("No files specified, exiting.")
+        exit()
+    elist = []
+    if args.total: elist += ['t']
+    if args.fc: elist += ['fc']
+    if args.ht: elist += ['ht']
+    if elist == []: elist = ['t']
+
+    for file in args.files:
+        spectrumDict = readSpectrum(file, args.energyIn[0], args.energyOut[0])
+        for stype in elist:
+            e = spectrumDict['e']
+            y = spectrumDict[stype]
+            with open(f'{file}.{stype}.csv', 'w+') as f:
+                f.write('energy,amplitude\n')
+                for i in zip(e, y):
+                    if i[0] > xrange[0] and i[0] < xrange[1]:
+                        f.write(f'{i[0]},{i[1]}\n')
+        print(file)
+
+if __name__ == "__main__":
+    main()
